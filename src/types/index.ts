4e44665150037ab1d7a9d4ec4b894efbd8feb21b
Rayane Achouchi <rayaneachouchi25@outlook.fr>
2025-09-02 13:22:19 +0200

    Created types files and added Procfile for Heroku deployment in production

---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+interface TelegramConfig {
+  botToken: string;
+  channelId: string;
+  errorChannelId?: string | undefined;
+  retryAttempts: number;
+  retryDelay: number;
+}
+
+interface HeliusConfig {
+  apiKey: string;
+  rpcUrl: string;
+  webhookUrl?: string | undefined;
+  retryAttempts: number;
+}
+
+interface TokenConfig {
+  mintAddress: string;
+  symbol: string;
+  decimals: number;
+}
+
+interface ServerConfig {
+  port?: string | number | undefined;
+  environment?: string | undefined;
+}
+
+interface FeaturesConfig {
+  enablePolling: boolean;
+  pollingInterval: number;
+  whaleThreshold: number;
+  batchWindow: number;
+  maxCacheSize: number;
+}
+
+interface Config {
+  telegram: TelegramConfig;
+  helius: HeliusConfig;
+  token: TokenConfig;
+  server: ServerConfig;
+  features: FeaturesConfig;
+}
+
+export type { Config, TelegramConfig, HeliusConfig, TokenConfig, ServerConfig, FeaturesConfig };
